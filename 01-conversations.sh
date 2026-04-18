891cfe62ebd2a88c00c36d640f4727e604d9e48f
Srikanth <srikanthr.br@gmail.com>
2026-04-18 22:59:18 +0530

    new file added- Variables

---
+++ 01-conversations.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=Sri
+PERSON2=Ram
+
+echo "$PERSON1: how are doing ?"
+echo ":$PERSON2 iam fine how abt u?"
+echo "$PERSON1: iam good thanks!"
+echo "$PERSON2: welcome! $PERSON1"
