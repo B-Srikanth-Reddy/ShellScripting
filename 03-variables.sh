16799a2e7ae3fbae468e8e016197a916747f11b2
Srikanth <srikanthr.br@gmail.com>
2026-04-19 12:58:02 +0530

    date

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,3 +3,11 @@
 START_TIME=$(date)
 
 echo "print start at : $START_TIME"
+
+################################
+
+START_TIME=$(date +%s)
+sleep 10 
+END_TIME=$(date +%s)
+
+echo (($START_TIME - $END_TIME))
