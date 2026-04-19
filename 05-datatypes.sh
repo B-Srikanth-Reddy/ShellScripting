9a9eb60d0dc6f7445674b89e9143ac6a033667b3
Srikanth <srikanthr.br@gmail.com>
2026-04-19 21:26:47 +0530

    cal

--- 05-datatypes.sh
+++ 05-datatypes.sh
@@ -2,5 +2,17 @@
 
 NUM1=35
 NUM2=10
+NUM3=Sri
+
+FRUITS=("Apple" "MAngo" "Carrot")
 
 echo "SUM is : $(($NUM1 + $NUM2))"
+
+echo "SUM with String : $(($NUM1 + $NUM3))"
+
+echo "FRUITS are: ${FRUITS[@]}"
+echo "FRUITS at 3rd: ${FRUITS[2]}"
+
+FRUITS[3]="test"
+
+echo "ALL Fruits: ${FRUITS[@]}"
