7823767b004e395de4d837011cc2c411ebc181a9
Srikanth <srikanthr.br@gmail.com>
2026-04-19 14:25:28 +0530

    cal

--- 04-specialVariables.sh
+++ 04-specialVariables.sh
@@ -7,6 +7,7 @@ echo "Msg from $PER1 to $PER2"
 
 echo "All Variables passed to script::$@"
 echo "no.of varaiables passed to script:: $#"
+echo "this is star:::$*"
 echo "Script Name :; $0"
 echo "PWD : $PWD"
 echo "Who is running Script:;; $USER"
